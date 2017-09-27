33dc44df6c13343ea00e38f58633a2e01a9f4d35
will <285721078@qq.com>
2017-09-27 14:39:41 +0800

    创建model-papi项目

---
+++ mdvn-model-papi/src/main/java/com/mdvns/mdvn/model/papi/MdvnModelPapiApplication.java
@@ -0,0 +1,12 @@
+package com.mdvns.mdvn.model.papi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdvnModelPapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdvnModelPapiApplication.class, args);
+	}
+}
