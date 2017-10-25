2dc6f2d49306a8985a6a00334a287552729ada2b
Frank23Woo <frank23woo@gmail.com>
2017-10-25 14:45:51 +0800

    update util for deployment

--- src/main/java/com/mdvns/mdvn/mdvn/util/DeployUtil.java
+++ src/main/java/com/mdvns/mdvn/mdvn/util/DeployUtil.java
@@ -1,5 +1,12 @@
 package com.mdvns.mdvn.mdvn.util;
 
+import org.apache.maven.model.Dependency;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.apache.maven.model.io.xpp3.MavenXpp3Writer;
+import org.codehaus.plexus.util.WriterFactory;
+import org.springframework.util.StringUtils;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,22 +15,22 @@ import java.util.Properties;
 public class DeployUtil {
     public static void main(String[] args) {
 
-        updateProperties("test",setPackageList());
-
+//        updateProperties("test",setPackageList());
+//        updatePomDependencyScope("spring-boot-starter-tomcat","",setPackageList());
 
     }
 
 
 
-    private static void updateProperties(String config, List<String> packList){
+    private static void updateProperties(String config, List<String> packageList){
         try {
             Properties pro = new Properties();
             InputStream in;
             String projPath = new File("").getAbsolutePath()+File.separator;
             String configPath = File.separator + "src" + File.separator+ "main" + File.separator + "resources" +File.separator +"application.properties";
             String attr = "spring.profiles.active";
-            for (int i = 0; i < packList.size(); i++) {
-                String packageName = packList.get(i);
+            for (int i = 0; i < packageList.size(); i++) {
+                String packageName = packageList.get(i);
                 String filePath = projPath + packageName + configPath;
                 in = new BufferedInputStream(new FileInputStream(filePath));
                 pro.load(in);
@@ -38,11 +45,47 @@ public class DeployUtil {
                     System.out.println("updated config: "+packageName+"-->"+attr+":"+pro.getProperty("spring.profiles.active"));
                 }
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+
+
+    private static void updatePomDependencyScope(String dependency, String scope, List<String> packageList){
+        if(StringUtils.isEmpty(scope)){
+            scope=null;
+        }
+        String projPath = new File("").getAbsolutePath();
+        for (int i = 0; i < packageList.size(); i++) {
+            File pomFile = new File(projPath +File.separator+ packageList.get(i)+File.separator+"pom.xml");
+            try {
+                MavenXpp3Reader reader = new MavenXpp3Reader();
+                Model model = reader.read(new FileReader(projPath +File.separator+ packageList.get(i)+File.separator+"pom.xml"));
+                List<Dependency> dependencies = model.getDependencies();
+                for (int j = 0; j < dependencies.size(); j++) {
+                    if(dependencies.get(j).getArtifactId().equals(dependency)){
+                        if(!StringUtils.isEmpty(dependencies.get(j).getScope()) && dependencies.get(j).getScope().equals(scope)){
+                            System.err.println(" current dependency scope is correct, no need to change");
+                        }else if(StringUtils.isEmpty(scope)  && StringUtils.isEmpty(dependencies.get(j).getScope())){
+                            System.err.println(" current dependency scope is also null, no need to change");
+                        }else{
+                            System.out.println(packageList.get(i)+" current dependency scope is "+dependencies.get(j).getScope());
+                            dependencies.get(j).setScope(scope);
+                            System.out.println(packageList.get(i)+" update dependency to "+dependencies.get(j).getScope());
+                        }
+                    }
+                }
+                model.setDependencies(dependencies);
+                MavenXpp3Writer pomWriter = new MavenXpp3Writer();
+                Writer fileWriter = WriterFactory.newXmlWriter(pomFile);
+                pomWriter.write(fileWriter,model);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     private static List<String> setPackageList(){
         List<String> packageList = new ArrayList<>();
         packageList.add("mdvn-model-papi");
@@ -60,10 +103,7 @@ public class DeployUtil {
         packageList.add("mdvn-task-papi");
         packageList.add("mdvn-task-sapi");
         return packageList;
-
     }
-
-
 
 
 
