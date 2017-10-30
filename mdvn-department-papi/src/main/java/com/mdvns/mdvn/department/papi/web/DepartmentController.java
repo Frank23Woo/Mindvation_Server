7efaaea5239a8c80ae7ef70ded80ce0371be63c8
will <285721078@qq.com>
2017-10-30 23:23:41 +0800

    跨域

--- mdvn-department-papi/src/main/java/com/mdvns/mdvn/department/papi/web/DepartmentController.java
+++ mdvn-department-papi/src/main/java/com/mdvns/mdvn/department/papi/web/DepartmentController.java
@@ -8,13 +8,11 @@ import com.mdvns.mdvn.department.papi.domain.RtrvDepartmentListRequest;
 import com.mdvns.mdvn.department.papi.domain.RtrvDepartmentListResponse;
 import com.mdvns.mdvn.department.papi.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = {"/department", "/v1/department"})
 public class DepartmentController {
